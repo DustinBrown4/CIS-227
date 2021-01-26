33821b908273b7abfb19a2bc371f15d71d0a25a0
Dustin Brown <77510407+DustinBrown4@users.noreply.github.com>
2021-01-25 19:51:56 -0500

    Update Assignment_1.cpp

--- Assignment_1.cpp
+++ Assignment_1.cpp
@@ -1,4 +1,4 @@
-// AreaApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Assignment_1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 #include <iostream>
 #include <iomanip> //for precision
